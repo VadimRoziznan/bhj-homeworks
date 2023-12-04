88d8185679cd2e41128cd64dce510955bce6dbce
RoziznanVadim <roziznan_v@mail.ru>
2023-12-04 19:12:33 +0300

    home_work

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,38 +1,62 @@
+// Добавляем обработчик события для полной загрузки документа
 document.addEventListener('DOMContentLoaded', () => {
+
+  // Получаем все элементы с классом "hole"
   const holes = document.getElementsByClassName('hole');
+
+  // Получаем элементы "score" и "misses" по их идентификаторам
   const scoreElem = document.getElementById('dead');
   const missesElem = document.getElementById('lost');
+
+  // Инициализируем переменные score и misses
   let score = 0;
   let misses = 0;
+
+  // Функция, которая получает элемент лунки по индексу
   const getHole = (index) => document.getElementById(`hole${index}`);
+
+  // Функция, вызываемая при клике на лунку
   const handleClick = (event) => {
+    // Получаем цель события (элемент, на котором произошло событие)
     const hole = event.target;
+
+    // Проверяем, содержит ли лунка класс "hole_has-mole"
     if (hole.classList.contains('hole_has-mole')) {
-      score++;
-      hole.classList.remove('hole_has-mole');
-      scoreElem.textContent = score;
+      score++; // Увеличиваем счет
+      hole.classList.remove('hole_has-mole'); // Удаляем класс "hole_has-mole" у лунки
+      scoreElem.textContent = score; // Обновляем текстовое содержимое элемента "scoreElem"
+
+      // Если набрано 10 очков, выводим сообщение о выигрыше и сбрасываем игру
       if (score === 10) {
         alert('Вы выиграли!');
         resetGame();
       }
     } else {
-      misses++;
-      missesElem.textContent = misses;
+      misses++; // Увеличиваем количество промахов
+      missesElem.textContent = misses; // Обновляем текстовое содержимое элемента "missesElem"
+
+      // Если набрано 5 промахов, выводим сообщение о проигрыше и сбрасываем игру
       if (misses === 5) {
         alert('Вы проиграли!');
         resetGame();
       }
     }
   };
+
+  // Регистрируем обработчики кликов для каждой лунки
   for (let i = 1; i <= holes.length; i++) {
     const hole = getHole(i);
     hole.addEventListener('click', handleClick);
   }
+
+  // Функция для сброса игры
   const resetGame = () => {
-    score = 0;
-    misses = 0;
-    scoreElem.textContent = score;
-    missesElem.textContent = misses;
+    score = 0; // Сбрасываем счет
+    misses = 0; // Сбрасываем количество промахов
+    scoreElem.textContent = score; // Обновляем текстовое содержимое элемента "scoreElem"
+    missesElem.textContent = misses; // Обновляем текстовое содержимое элемента "missesElem"
+
+    // Восстанавливаем все лунки с классом "hole_has-mole"
     for (let i = 1; i <= holes.length; i++) {
       const hole = getHole(i);
       hole.classList.add('hole_has-mole');
@@ -40,3 +64,6 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
 });
+
+
+
