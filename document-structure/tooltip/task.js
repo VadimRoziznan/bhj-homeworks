287cd5c27959a6de1176502a2732d0e3cbeea9ad
Vadim Roziznan <roziznan_v@mail.ru>
2023-12-22 20:27:55 +0300

    hw

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,23 +1,65 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
 
-hasTooltip.forEach(element => {
-    element.addEventListener('mouseover', function(event) {
-        event.preventDefault();
-        
-        
-        /*const newTag = document.createElement('div');
-        newTag.className = 'tooltip';
-        newTag.textContent = element.title;
 
-        console.log(newTag)*/
+hasTooltip.forEach(element => {
+    element.addEventListener('mouseenter', function(event) {
+        event.preventDefault();
+        const tooltip = document.querySelector('.tooltip');
+
+        if (element.title) {
+            tooltip.textContent = element.title;
+            element.title = ''
+        }
+        
+
+        console.log(element.querySelector('.tooltip'))
+
+        /*if (element.title) {
+            const newTag = document.createElement('div');
+        
+            document.body.appendChild(newTag);
+            newTag.className = 'tooltip';
+            newTag.textContent = element.title;
+
+            element.title = ''
+        }*/
+
+        element.onclick = () => {
+            const child = element.querySelector('.tooltip');
+            if (child.classList.contains('tooltip_active')) {
+                child.classList.remove('tooltip_active')
+            } else {
+                child.classList.add('tooltip_active')
+            }
+            
+        }
+
+    element.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        /*element.style = 'pointer-events:auto;'*/
+        
+    })
+        
+
+        /*title = element.title;
+        
+        
+        element.href = ''
+        
+
         if (element.title.length > 0) {
             const ppp = element.title
             element.title = ''
-        }
-        element.onclick = () => {
+        } else {
             element.title = ppp
         }
-        console.log(element['data-tipsy'])
+        element.onclick = () => {
+            event.preventDefault();
+            
+            console.log('lfsdlsfdlkdsfdlk')
+        }*/
+        
 
         
         
