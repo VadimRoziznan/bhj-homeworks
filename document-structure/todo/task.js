ba24af1a1cd28eaae040313aa1801eff50fe9529
Vadim Roziznan <roziznan_v@mail.ru>
2023-12-24 19:27:21 +0300

    hw

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,17 +2,25 @@ const tasksAdd = document.querySelector('.tasks__add')
 const form = document.querySelector('form')
 const tasksList = document.querySelector('#tasks__list');
 const newDiw = document.createElement('div');
+const input = form.querySelector('#task__input')
 
-tasksAdd.addEventListener('click', (event) => {
-    tasksAdd.setAttribute('type', 'button');
+tasksAdd.setAttribute('type', 'button');
+
+tasksAdd.onclick = () => {
+    const newDiw = document.createElement('div');
     
-    const formData = new FormData(form);
-    const input = form.querySelector('#task__input')
-
-    tasksAdd.onclick = () => {
-        const newDiw = document.createElement('div');
-        newDiw.innerHTML = `<div class="task"><div class="task__title">Сходить в магазин</div><a href="#" class="task__remove">&times;</a></div>`;
+    if (input.value) {
+        newDiw.innerHTML = `<div class="task"><div class="task__title">${input.value}</div><a href="#" class="task__remove">&times;</a></div>`;
         tasksList.appendChild(newDiw);
+        input.value = ''
+        const taskRemove = document.querySelectorAll('.task__remove');
+
+        taskRemove.forEach(link => {
+            link.addEventListener('click', () => {
+                parent_element = link.closest('.task')
+                parent_element.remove();
+            });
+          });
+        
     }
-    /*console.log(input.value)*/
-})
+}
